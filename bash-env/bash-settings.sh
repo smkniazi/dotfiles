9fcac293af92f21d3f69456d987244039d4a228c
Salman Niazi <smkniazi@kth.se>
2017-04-09 17:16:56 +0200

    Auto generated commit msg.

--- bash-env/bash-settings.sh
+++ bash-env/bash-settings.sh
@@ -1,4 +1,4 @@
 #Using this, consecutive duplicate commands, invocations of ls, plus calls to the bg, fg and exit built-ins will not be appended to the history list. 
 export HISTIGNORE="&:ls:ls *:[bf]g:exit" 
-bind 'set show-all-if-ambiguous on'
-bind 'TAB:menu-complete'
+#bind 'set show-all-if-ambiguous on'
+#bind 'TAB:menu-complete'
