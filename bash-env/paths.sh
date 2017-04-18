9487c5e7c241aa23cf767319d05f37f8eac57b10
Salman Niazi <smkniazi@kth.se>
2017-04-18 16:36:48 +0200

    Auto generated commit msg.

--- bash-env/paths.sh
+++ bash-env/paths.sh
@@ -1,27 +1,36 @@
-export JAVA_HOME=/home/salman/program_files/jdk
-export PATH=$PATH:/home/salman/program_files/jdk/bin
+export_path(){
+	if ! grep -q $1 <<< $PATH; then
+		if [ -d "$1" ]; then
+			export PATH=$PATH:$1
+		fi
+	fi
+}
+ 
+export_ld_lib_path(){
+	if ! grep -q $1 <<< $LD_LIBRARY_PATH; then
+		if [ -d "$1" ]; then
+			export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$1
+		fi
+	fi
+}
 
-export M2_HOME=/home/salman/program_files/maven
-export PATH=$PATH:/home/salman/program_files/maven/bin
+export_ld_lib_path ~/program_files/protobuf/src/.libs
+export_ld_lib_path ~/program_files/clusterj/
 
-export PATH=$PATH:/home/salman/program_files
+export_path ~/program_files
+export_path ~/program_files/jdk/bin
+export_path ~/program_files/maven/bin
+export_path ~/program_files/glark-1.7.10/bin
+export_path ~/program_files/protobuf/src
+export_path ~/program_files/set-pic-time
+export_path ~/program_files/idea/bin
+export_path ~/program_files/scala/bin
+export_path ~/program_files/sbt/bin
+export_path ~/program_files/latexdiff
+export_path ~/usr/local/texlive/2016/bin/x86_64-linux
+export_path ~/Dropbox
 
-export PATH=$PATH:/home/salman/program_files/glark-1.7.10/bin
-
-export PATH=$PATH:/home/salman/program_files/protobuf/src
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/salman/program_files/protobuf/src/.libs
 
 export LIBNDBPATH=/home/salman/program_files/clusterj
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/salman/program_files/clusterj/
-
-export PATH=$PATH://home/salman/program_files/set-pic-time
-
-export PATH=$PATH://home/salman/program_files/idea/bin
-
-export PATH=$PATH:/home/salman/program_files/scala/bin
-
-export PATH=$PATH:/home/salman/program_files/sbt/bin
-
-export PATH=$PATH:/home/salman/program_files/latexdiff
-
-export PATH=$PATH:/usr/local/texlive/2016/bin/x86_64-linux
+export JAVA_HOME=/home/salman/program_files/jdk
+export M2_HOME=/home/salman/program_files/maven
