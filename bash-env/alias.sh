ad62841aa920e08d0a2ee72b5bc7ccc0d8befbb7
Salman Niazi <smkniazi@kth.se>
2017-06-27 09:55:30 +0200

    Auto generated commit msg.

--- bash-env/alias.sh
+++ bash-env/alias.sh
@@ -1,3 +1,16 @@
+echoColor() {
+	Black='\033[0;30m'        # Black
+	Red='\033[0;31m'          # Red
+	Green='\033[0;32m'        # Green
+	Yellow='\033[0;33m'       # Yellow
+	Blue='\033[0;34m'         # Blue
+	Purple='\033[0;35m'       # Purple
+	Cyan='\033[0;36m'         # Cyan
+	White='\033[0;37m'        # White
+	NC='\033[0m' # No Color
+	printf "${Cyan}$1${NC}\n"
+}
+
 create_path_alias(){
 	if [ -d "$2" ]; then
 		eval "alias $1=\"cd $2\""
@@ -63,7 +76,31 @@ ta (){
 }
 
 
+gcp() {
+	if [ "$#" -eq 1 ]; then
+		git commit -am "$1" && git push
+	else
+		joke=""
+		#joke="$(wget 'http://api.icndb.com/jokes/random' -qO- | grep -Po '"joke":.*?[^\\]",' |  sed -e 's/\"//g' | sed -e 's/joke: //g' | sed -e 's/\"//g')"
+
+		joke="Auto generated commit msg.  $joke"
+		git commit -am "$joke" && git push
+	fi
+}
+
+ph () {
+	pushd .
+	echoColor "Hops"
+	cd /home/salman/code/hops/hops && git push 
+	echoColor "Hops-Metadata-Dal"
+	cd /home/salman/code/hops/hops-metadata-dal && git push
+	echoColor "Hops-Metadata-Dal-Impl-NDB"
+	cd /home/salman/code/hops/hops-metadata-dal-impl-ndb && git push
+	popd
+}
+
 bh (){
+	pushd .
 	mvn -T 1C $@ install -Dmaven.test.skip=true -f ~/code/hops/hops-gpu-management/pom.xml
 
 	mvn -T 1C $@ install -Dmaven.test.skip=true -f ~/code/hops/hops-gpu-management-impl-nvidia/pom.xml
@@ -76,6 +113,7 @@ bh (){
 
 	#temporarily change pwd
 	cd ~/code/hops/hops && mvn -T 1C $@ test-compile
+	popd
 }
 
 mi () {
@@ -86,22 +124,7 @@ mig () {
 	mvn $@ install -Dmaven.test.skip=true generate-sources -Pndb
 }
 
-ph () {
-	cd /home/salman/code/hops/hops && git push && cd /home/salman/code/hops/hops-metadata-dal && git push && cd /home/salman/code/hops/hops-metadata-dal-impl-ndb && git push
-}
 
-
-gcp() {
-	if [ "$#" -eq 1 ]; then
-		git commit -am "$1" && git push
-	else
-		joke=""
-		#joke="$(wget 'http://api.icndb.com/jokes/random' -qO- | grep -Po '"joke":.*?[^\\]",' |  sed -e 's/\"//g' | sed -e 's/joke: //g' | sed -e 's/\"//g')"
-
-		joke="Auto generated commit msg.  $joke"
-		git commit -am "$joke" && git push
-	fi
-}
 
 s2 () {
 #ssh -A -t nzo@cloud1.sics.se ssh -A -t salman@salman2.sics.se
