f0f174cb3c15f6c62611edc75cb45a97817df149
Salman Niazi <smkniazi@kth.se>
2017-07-13 16:13:52 -0400

    Updates

--- bash-env/bash-settings.sh
+++ bash-env/bash-settings.sh
@@ -1,7 +1,7 @@
 #Using this, consecutive duplicate commands, invocations of ls, plus calls to the bg, fg and exit built-ins will not be appended to the history list. 
-export HISTIGNORE="&:ls:ls *:[bf]g:exit" 
+#export HISTIGNORE="&:ls:ls *:[bf]g:exit" 
 #bind 'set show-all-if-ambiguous on'
 #bind 'TAB:menu-complete'
 
 #disable ctrl+s which freezes the terminal
-stty -ixon
+#stty -ixon
