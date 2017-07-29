c751b3e29444274b60639599ab78d88ab1416252
Salman Niazi <smkniazi@kth.se>
2017-07-29 12:03:59 +0200

    Auto generated commit msg.

--- bash-env/bash-settings.sh
+++ bash-env/bash-settings.sh
@@ -10,3 +10,14 @@
 #0;47;32m light gray background and green foreground
 export PS1="\[\033[32m\][\u@\h \[\033[32m\]\W\[\033[33m\]\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/')\[\033[32m\]]$\[\033[00m\] "
 
+
+#combine history across multiple sessions
+#http://experimentswithlinuxrelatedtech.blogspot.se/2014/07/combine-bash-history-across-multiple.html
+export HISTFILE=~/.bash_history
+export HISTFILESIZE=500000
+export HISTSIZE=10000
+export HISTCONTROL=ignoredups:erasedups
+shopt -s histappend
+unset PROMPT_COMMAND
+export PROMPT_COMMAND="history -n;history -w;history -c;history -r;$PROMPT_COMMAND"
+
