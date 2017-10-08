cd953d9b7411df1021bb77395890ff6902fc72af
Salman Niazi <smkniazi@kth.se>
2017-10-08 09:43:57 +0200

    Auto generated commit msg.

--- myscripts/scripts/volume.sh
+++ myscripts/scripts/volume.sh
@@ -2,3 +2,6 @@
 #example 10% , 10%+,  10%-
 amixer -q -D pulse sset Master $1
 #echo $1 >> /tmp/switcher.log
+#curval=$(amixer sget Master | grep "Front Left.*[0-9]*%" | grep -oh "[0-9]*%" | grep -oh "[0-9]*")
+#pkill zenity
+#timeout 1 zenity --scale --text test --min-value 0 --max-value 100 --value $curval
