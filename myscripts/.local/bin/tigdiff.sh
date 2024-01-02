0daf24ce792453d04f7f39983314962cd4ecfd4c
Salman Niazi <salman@logicalclocks.com>
2024-01-02 16:01:39 +0100

    Auto generated commit msg.

--- myscripts/.local/bin/tigdiff.sh
+++ myscripts/.local/bin/tigdiff.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+echo $@ >> /tmp/log
 if [ "0000000000000000000000000000000000000000" = "$1" ]; then
-    git difftool -y "HEAD" $2
+    git difftool -y "HEAD" -- $2
 else
-    git difftool -y $1~1 $1 $2
+    git difftool -y $1~1 $1 -- $2
 fi
 #echo "$1 $2 $3"
